da4382c0a4b0aaf43f21df992f823ced8c64c529
Aleksei Sosov <forgithublesh@yandex.ru>
2026-02-28 14:29:01 +0400

    [messaging-contracts] feat base_messages.py added logical_id and created_at to BaseMessage

--- src/messaging_contracts/messaging_contracts/common/base_messages.py
+++ src/messaging_contracts/messaging_contracts/common/base_messages.py
@@ -1,8 +1,14 @@
-from pydantic import BaseModel, ConfigDict
+from datetime import datetime, timezone
+from uuid import UUID
+
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class BaseMessage(BaseModel):
     model_config = ConfigDict(frozen=True, extra="forbid")
+
+    logical_id: UUID
+    created_at: datetime = Field(default_factory=lambda: datetime.now(tz=timezone.utc))
 
 
 class Event(BaseMessage):
